718143eb35cfffae37f90b1d8de347ba54efa040
unknown <you@example.com>
2023-06-24 00:37:18 +0700

    update code 24/6

--- SPCK/trip_planner.js
+++ SPCK/trip_planner.js
@@ -1,47 +1,45 @@
-// function button(){
-//     document.getElementById("location").innerHTML="";
-//     document.getElementById("plan").innerHTML="";
-
-//     // hiển thị địa điểm du lịch
-//     let destination=document.getElementById("destination").value
-//     let location = document.getElementById("location")
-//     location.innerText=destination
-
-//     // hiển thị số ngày
-//     let day = document.getElementById("days").value
-//     for (let i = 1; i<=day; i++){
-//         let div = document.createElement("div")
-//         let img = document.createElement("img")
-//         img.innerHTML="https://img.icons8.com/?size=512&id=WwP9ajtrQdZj&format=png"
-//         document.getElementById("plan").appendChild(div)
-//         div.appendChild(img)
-//         div.innerText= "day "+ i +":" + img
-//     }
-// }
-
-
-
-
-
 function destination(destination) {
     document.getElementById("location").innerHTML = " ";
     let h3 = document.createElement("h3");
-    h3.innerText = "Ho Chi Minh, Vietnam";  //destination
+    h3.innerText = destination;
     document.getElementById("location").appendChild(h3);
 }
 
-// function button() { 
-//     document.getElementById("plan").innerHTML = " ";
-//     let day = document.getElementById("days").value
-//     for (let i = 1; i<=day; i++){
-//         let div = document.createElement("div")
-//         let img = document.createElement("img")
-//         img.style.height='50px'
-//         img.setAttribute("src", "https://img.icons8.com/?size=512&id=WwP9ajtrQdZj&format=png");
-//         document.getElementById("plan").appendChild(div)
-//          div.appendChild(img);
-//         div.innerText= "Day "+ i +":" + img.appendChild("https://img.icons8.com/?size=512&id=WwP9ajtrQdZj&format=png")
-// }
-// }
+function plan(activities) {
+    document.getElementById("plan").innerHTML = " ";
+    let day = document.getElementById("day").value
+
+    for (let i = 1; i <= day; i++) {
+        let divmain = document.createElement("div")
+        document.getElementById("plan").appendChild(divmain)
+        let div1 = document.createElement("div")
+        let div2 = document.createElement("div")
+        divmain.appendChild(div1);
+        divmain.appendChild(div2);
+        divmain.style.display = "flex"
+        divmain.style.flexDirection = "column"
+        div1.innerText = "Day " + i + ":"
+        div2.innerText = activities
+    }
+}
 
 
+function button() {
+    let destination = document.getElementById("destination").value;
+    let day = document.getElementById("day").value;
+
+    const url = `https://ai-trip-planner.p.rapidapi.com/current.json?destination=${destination}&days=${day}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'e5d99d8ea1msh823c45fa45d01f4p1f77a5jsn73eb789f46dc',
+            'X-RapidAPI-Host': 'ai-trip-planner.p.rapidapi.com'
+        }
+    };
+    fetch(url, options).then((res) => {
+        return res.json();
+    }).then((data) => {
+        destination(key);
+        plan(data.plan.activities)
+    })
+}
